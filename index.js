61c7eaa8b34d3d2ad7ae3e9eaf76f7fedb3410a9
vitriolfox <vitriol.fox@gmail.com>
2017-11-09 09:14:25 +0100

    students API

--- index.js
+++ index.js
@@ -1,35 +1,119 @@
 const express = require('express');
 const students = require('./data');
 const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const methodOverride = require('method-override');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+// handlebars
+app.set('views', './views/');
+app.set('view engine', 'hbs');
+app.engine('hbs', require('hbs').__express);
+
+hbs.registerHelper('currentPage', (pages, pageNumber, options) => {
+  if (pages === pageNumber) {
+    return options.fn(pageNumber);
+  }
+  return options.inverse(this);
+});
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/students', (request, response) => {
-  response.json({students: students});
+  let limit = parseInt(request.query.limit) || 5;
+  let pageFrom;
+  let pageEnd;
+  let pageNumber = parseInt(request.query.page) || 1;
+  let next = pageNumber + 1;
+  let prev = pageNumber - 1;
+  let firstPage = pageNumber > 1;
+  let lastPage = pageNumber < students.length / limit;
+
+  let pages = [];
+  let n = 1;
+  for (let i = 0; i < students.length / limit; i++) {
+    pages[i] = n;
+    n++;
+  }
+
+  if (parseInt(request.query.page) <= 1 || request.query.page === undefined) {
+    pageFrom = 0;
+    pageEnd = limit;
+  } else {
+    pageFrom = (parseInt(request.query.page) - 1) * limit;
+    pageEnd = pageFrom + limit;
+  }
+  let studentList = students.slice(pageFrom, pageEnd);
+  response.format({
+    'text/html': () => response.render('students/index', {
+      students: studentList,
+      pageNumber: pageNumber,
+      next: next,
+      prev: prev,
+      firstPage: firstPage,
+      lastPage: lastPage,
+      pages: pages
+
+    }),
+    'application/json': () => response.json({students: studentList})
+  });
+});
+
+app.get('/students/form', (request, response) => {
+  response.render('students/form');
+});
+
+app.get('/students/:name/update', (request, response) => {
+  let student = students.find(student => student.name === request.params.name);
+  response.render('students/update', {student: student});
 });
 
 app.get('/students/:name', (request, response) => {
   let student = students.find(student => student.name === request.params.name);
-  response.json({student: student});
+  response.format({
+    'text/html': () => response.render('students/show', {student: student}),
+    'application/json': () => { response.json({student: student}); }
+  });
 });
 
-app.post('/students', (request, response) => {
+app.get('/students', function (request, response) {
+  response.format({
+    'text/html': function () {
+      response.render('students/form', {
+        student: students
+      });
+    },
+    'applicaton/json': function () {
+      response.json({students: students});
+    }
+  });
+});
+
+app.post('/students', function (request, response) {
   const newStudent = {
     name: request.body.name,
     sex: request.body.sex,
     score: request.body.score,
     age: request.body.age
   };
+
   students.push(newStudent);
-  response.json({student: newStudent});
+
+  response.format({
+    'text/html': function () {
+      response.redirect('/students');
+    },
+    'application/json': function () {
+      response.json({student: newStudent});
+    }
+  });
 });
 
-// update adott tömbelemet frissít név szerint
-// delete, post keveréke
-// handlebars, templatenek, lapozás
-app.post('/students/:name', (request, response) => {
+app.put('/students/:name', (request, response) => {
   const updateStudent = {
     name: request.body.name,
     sex: request.body.sex,
@@ -39,14 +123,27 @@ app.post('/students/:name', (request, response) => {
   let indexOfUpdateStudent = students.indexOf(students.find(student => student.name === request.params.name));
   students.splice(indexOfUpdateStudent, 1);
   students.splice(indexOfUpdateStudent, 0, updateStudent);
-  response.json({student: students});
+
+  response.format({
+    'text/html': function () {
+      response.redirect('/students/' + updateStudent.name);
+    },
+    'application/json': function () {
+      response.json({student: updateStudent});
+    }
+  });
 });
 
 app.delete('/students/:name', (request, response) => {
   students.splice(students.indexOf(students.find(student => student.name === request.params.name)), 1);
-  response.json({students: students});
+  response.format({
+    'text/html': function () {
+      response.redirect('/students/');
+    },
+    'application/json': function () {
+      response.json({students: students});
+    }
+  });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(3000, () => console.log('Server is running on port 3000'));
